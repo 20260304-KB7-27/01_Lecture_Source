809b7502332fb18d9e00fadf1c73bafbdaf3301e
Ohgiraffers-bear <bear.ohgiraffers@gmail.com>
2026-04-06 14:57:36 +0900

    04/06

--- 03_Vue/01_basic/12_pinia/pinia_lecture/src/components/Counter.vue
+++ 03_Vue/01_basic/12_pinia/pinia_lecture/src/components/Counter.vue
@@ -6,11 +6,14 @@
 </template>
 
 <script setup>
-import { useCounterStore } from '@/stores/counter';
+import { useCounterPersistStore } from '@/stores/persist/persist_counter';
+
+// import { useCounterStore } from '@/stores/counter';
 
 // State를 꺼낼때는 구조분해 할당을 하게되면, 반응형이 깨짐
-const counter = useCounterStore(); // 객체 자체를 꺼내서 쓰는 방식
-const { count, increment, reset } = useCounterStore(); // 구조분해할당으로 꺼내는 방식
+const counter = useCounterPersistStore(); // 객체 자체를 꺼내서 쓰는 방식
+
+const { count, increment, reset } = useCounterPersistStore(); // 구조분해할당으로 꺼내는 방식
 </script>
 
 <style scoped></style>
