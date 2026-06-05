71689392a9c1909f33b4fc4a31dd68f002ebaefb
Ohgiraffers-bear <bear.ohgiraffers@gmail.com>
2026-06-05 15:21:32 +0900

    06/05

---
+++ 05_DATABASE/07_Mongo/mongo_pojo/src/main/java/app/domain/Todo.java
@@ -0,0 +1,22 @@
+package app.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+
+/*
+* POJO (Plan Old Java Object)
+* -> 순수한 Java로 만들어진 객체
+* -> lombok은 단순히 자바로 이루어진 코드를 생성하는것이기때문에 상관 X
+* */
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Todo {
+    private ObjectId id; // mongodb에도 ID가 객체형태로 저장됨
+    private String title;
+    private String desc;
+    private boolean done;
+}
