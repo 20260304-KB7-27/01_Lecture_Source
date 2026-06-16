51b3d0797d07f6e93e8c8dec47c1492681ba66d2
Ohgiraffers-bear <bear.ohgiraffers@gmail.com>
2026-06-16 16:14:43 +0900

    06/16

--- 07_Spring/spring-mvc/src/main/java/org/scoula/exception/ExceptionAdvice.java
+++ 07_Spring/spring-mvc/src/main/java/org/scoula/exception/ExceptionAdvice.java
@@ -4,6 +4,9 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+import javax.servlet.http.HttpServletRequest;
 
 /*
 * 전역처리를 하기 위한 어노테이션
@@ -17,6 +20,14 @@ public class ExceptionAdvice {
     public String exceptionHandle(Exception e, Model model) {
         model.addAttribute("errorMessage", e.getMessage());
         return "error_page";
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public String exceptionHandle2(NoHandlerFoundException e, Model model, HttpServletRequest request) {
+
+        model.addAttribute("uri", request.getRequestURI());
+
+        return "custom404";
     }
 
 }
