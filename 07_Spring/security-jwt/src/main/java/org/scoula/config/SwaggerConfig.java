7135fe745fcc33361ee5590d19f2538b867d5eb9
Ohgiraffers-bear <bear.ohgiraffers@gmail.com>
2026-06-30 17:40:58 +0900

    06/30

--- 07_Spring/security-jwt/src/main/java/org/scoula/config/SwaggerConfig.java
+++ 07_Spring/security-jwt/src/main/java/org/scoula/config/SwaggerConfig.java
@@ -3,14 +3,19 @@ package org.scoula.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
-import org.w3c.dom.DocumentType;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.List;
 
 @Configuration
 @EnableSwagger2 // swagger 기능 활성화
@@ -27,7 +32,6 @@ public class SwaggerConfig {
                 .build();
     }
 
-
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
@@ -36,20 +40,30 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
                 .paths(PathSelectors.any())
                 .build()
-                .apiInfo(apiInfo());
+                .apiInfo(apiInfo())
+                // Authorize 버튼을 만들어 보안스키마를 등록할 수 있는 기능
+                .securitySchemes(List.of(apiKey()))
+                // 등록한 스키마를 실제 API 경로에 적용
+                .securityContexts(List.of(securityContext()));
     }
 
-    @Bean
-    public Docket api2() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .groupName("02_공통응답테스트용")
-                .select()
-                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
-                .paths(PathSelectors.ant("/test/board/**")) // 얘네만 문서로 만들겠다
-                .build()
-                .apiInfo(new ApiInfoBuilder()
-                        .title("🍣 공통응답 테스트 페이지")           // 문서 제목
-                        .description("Board 단건조회로 공통응답테스트를 할 수 있습니다.") // 문서 설명
-                        .build());
+    // schemes
+    private ApiKey apiKey() {
+        return new ApiKey("Authorization", "Authorization", "header");
     }
+
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        return List.of(new SecurityReference("Authorization", new AuthorizationScope[]{authorizationScope}));
+    }
+
+    // Swagger에 메소드마다 자물쇠 아이콘 표시
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(PathSelectors.any())
+                .build();
+    }
+
 }
--- 07_Spring/security-jwt/src/main/java/org/scoula/security/config/SecurityConfig.java
+++ 07_Spring/security-jwt/src/main/java/org/scoula/security/config/SecurityConfig.java
@@ -3,7 +3,11 @@ package org.scoula.security.config;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.mybatis.spring.annotation.MapperScan;
+import org.scoula.security.filter.AuthenticationErrorFilter;
+import org.scoula.security.filter.JwtAuthenticationFilter;
 import org.scoula.security.filter.JwtUsernamePasswordAuthenticationFilter;
+import org.scoula.security.handler.CustomAccessDeniedHandler;
+import org.scoula.security.handler.CustomAuthenticationEntryPoint;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -28,8 +32,8 @@ import org.springframework.web.filter.CharacterEncodingFilter;
 import java.util.Arrays;
 
 /*
-* Spring Security의 보안 설정 클래스
-* */
+ * Spring Security의 보안 설정 클래스
+ * */
 @Configuration
 @EnableWebSecurity // 필터체인 활성화
 @RequiredArgsConstructor
@@ -39,7 +43,10 @@ import java.util.Arrays;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final UserDetailsService userDetailsService;
-
+    private final JwtAuthenticationFilter jwtAuthenticationFilter; // JWT 인증필터
+    private final AuthenticationErrorFilter authenticationErrorFilter; // jwt 예외 필터
+    private final CustomAuthenticationEntryPoint authEntryPoint; // 미인증 요청 처리
+    private final CustomAccessDeniedHandler customAccessDeniedHandler; // 미인가 처리
     @Autowired
     private JwtUsernamePasswordAuthenticationFilter jwtUsernamePasswordAuthenticationFilter;
 
@@ -52,15 +59,25 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     /*
-    * CSRF : 로그인한 사용자를 악의적인 사이트에서 몰래 요청을 보내게하는 공격
-    * */
+     * CSRF : 로그인한 사용자를 악의적인 사이트에서 몰래 요청을 보내게하는 공격
+     * */
     // 시큐리티 세팅할 공간
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         // CSRF 필터 앞에다 encodingFilter를 놓겠다.
         http.addFilterBefore(encodingFilter(), CsrfFilter.class)
+                // JWT 예외필터 -> JWT 인증필터 -> 로그인 필터 -> Usernamepassword필터
+                .addFilterBefore(authenticationErrorFilter,
+                        UsernamePasswordAuthenticationFilter.class) // JWT 예외 필터
+                .addFilterBefore(jwtAuthenticationFilter,
+                        UsernamePasswordAuthenticationFilter.class) // JWT 인증 필터
                 .addFilterBefore(jwtUsernamePasswordAuthenticationFilter,
-                        UsernamePasswordAuthenticationFilter.class);
+                        UsernamePasswordAuthenticationFilter.class); // 로그인 필터
+
+        // 예외 핸들러 등록
+        http.exceptionHandling()
+                .authenticationEntryPoint(authEntryPoint)// 인증이 안된 경우의 핸들러
+                .accessDeniedHandler(customAccessDeniedHandler); // 인가 문제 핸들러
 
         // CORS 설정 추가
         http.cors();
@@ -74,11 +91,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         // URL별 접근 권한 설정
         http.authorizeRequests()
                 .antMatchers("/security/all")
-                    .permitAll()// 모든 권한 접근 허용
+                .permitAll()// 모든 권한 접근 허용
                 .antMatchers("/security/admin")
-                    .access("hasRole('ROLE_ADMIN')")
+                .access("hasRole('ROLE_ADMIN')")
                 .antMatchers("/security/member")
-                    .access("hasAnyRole('ROLE_ADMIN','ROLE_MEMBER')");
+                .access("hasAnyRole('ROLE_ADMIN','ROLE_MEMBER')")
+                .anyRequest().authenticated(); // 인증된 사람들 (인증객체가 있는 상태)
 
     }
 
@@ -106,7 +124,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(
                         "/assets/**",
                         "/*", // 루트경로 바로 아래 /login, /member
-                        "/api/member/**" // /api/member 하위 경로 제외
+                        "/api/member/**", // /api/member 하위 경로 제외
+                        "/swagger-ui.html",
+                        "/swagger-resources/**",
+                        "/webjars/**",
+                        "/v2/api-docs"
                 );
     }
 
