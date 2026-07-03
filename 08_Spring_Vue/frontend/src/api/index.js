6bf0cb5e634415d9ef66f77647c9a0d85ff997ab
Ohgiraffers-bear <bear.ohgiraffers@gmail.com>
2026-07-03 16:03:26 +0900

    07/03

--- 08_Spring_Vue/frontend/src/api/index.js
+++ 08_Spring_Vue/frontend/src/api/index.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import { useAuthStore } from '@/stores/auth';
-import { useRouter } from 'vue-router';
+// import { useRouter } from 'vue-router';
+// useRouter Vue컴포넌트 전용
+import router from '@/router';
 
 const instance = axios.create({
   timeout: 1000, // 요청 타임아웃
@@ -22,7 +24,7 @@ instance.interceptors.response.use(
     if (error.response?.status === 401) {
       console.log('인증 실패 (401) - 토큰이 유효하지 않음');
       const { logout } = useAuthStore();
-      const router = useRouter();
+
       logout(); // 기존토큰 지워버리기
 
       router.push('/auth/login'); // 로그인 페이지로 이동
